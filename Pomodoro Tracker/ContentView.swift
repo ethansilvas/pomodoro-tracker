e3ffacb40f95564a29435c94c414d4a596dff851
Ethan Silvas <ethansilvas2@gmail.com>
2022-07-28 14:37:04 -0700

    Initial Commit

---
+++ Pomodoro Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pomodoro Tracker
+//
+//  Created by Ethan Silvas on 7/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pomodoro Tracker/Pomodoro_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TrackerApp.swift
+//  Pomodoro Tracker
+//
+//  Created by Ethan Silvas on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
